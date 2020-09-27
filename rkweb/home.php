ac650eeda6b78205f04077d17754b1e96c5c9fc0
ranjitkisku <71980006+ranjitkisku@users.noreply.github.com>
2020-09-27 09:20:35 -0700

    Add files via upload

---
+++ rkweb/home.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+
+
+
+
+
+
+?>
+<!DOC TYPE! html>
+<html align="center"> <b>
+<head align="center">Welcome To St. Philip's High School And College <head/>
+
+<body>
+<h1 align="center"> Please Register</h1>
+<form  action="#" method="POST" align="center"> 
+<label for="fn" align="center"> First Name </label>
+<br>
+<input type="text" name="fn" id="fn" placeholder=" First name"></input>
+<br>
+<label for="ln"> Last Name</label>
+<br>
+<input type="text" name"ln" id="ln" placeholder="Last Name"></input>
+<br>
+<label for="age"> Age</label>
+<br>
+<input type="number" name="age" id="nm" placeholder=" Enter your Age"></input>
+<br>
+<label for="em">email</label>
+<br>
+<input type="email" name"em" id="em" placeholder=" Enter your Email"></input>
+<br>
+<labe for="password">Password</label>
+<br>
+<input type="password" name="pass" id="pass" placeholder="Enter Your Password"></input>
+<br>
+<input type "button" value="Register" name="reg"></input>
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+</html>
---
+++ rkweb/reg.php
@@ -0,0 +1,71 @@
+<?php 
+
+
+
+
+
+
+
+
+?>
+
+
+
+<html>
+<!DOC TYPE:html>
+<head lang="en">
+<meta charset="utf8">
+<title> Registration Form</title>
+<style>
+h1{
+color:grey;
+font_style :italic;
+}h1
+</style>
+
+
+
+</head>
+
+<body></body>
+<h1> Please Register</h1>
+<form  action="#" method="POST"> 
+<label for="fn"> First Name </label>
+<br>
+<input type="text" name="fn" id="fn" placeholder=" First name"></input>
+<br>
+<label for="ln"> Last Name</label>
+<br>
+<input type="text" name"ln" id="ln" placeholder="Last Name"></input>
+<br>
+<label for="age"> Age</label>
+<br>
+<input type="number" name="age" id="nm" placeholder=" Enter your Age"></input>
+<br>
+<label for="em">email</label>
+<br>
+<input type="email" name"em" id="em" placeholder=" Enter your Email"></input>
+<br>
+<labe for="password">Password</label>
+<br>
+<input type="password" name="pass" id="pass" placeholder="Enter Your Password"></input>
+<br>
+<input type " submit" value="Register" name="reg"></input>
+
+
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+ </html>
+
